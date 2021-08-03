c250bbf2f93e7dcdd683ff99062cd0fe379ee694
jrc03c <jrc03c@pm.me>
2021-08-03 13:28:09 -0500

    ...

---
+++ vue-component-css-mixin.js
@@ -0,0 +1,47 @@
+const dict = {}
+
+VueComponentCSSMixin = {
+  install: function (Vue, options) {
+    Vue.mixin({
+      beforeMount: async function () {
+        const self = this
+        const tagKey = self.$options.name + "-style-tag"
+        const countKey = tagKey + "-count"
+
+        if (!dict.hasOwnProperty(countKey)) dict[countKey] = 0
+        dict[countKey]++
+
+        const styleTag = document.createElement("style")
+
+        if (self.css) {
+          if (dict.hasOwnProperty(tagKey)) return
+          dict[tagKey] = styleTag
+          styleTag.innerHTML = self.css
+          document.head.appendChild(styleTag)
+        }
+      },
+
+      destroyed: function () {
+        const self = this
+        const tagKey = self.$options.name + "-style-tag"
+        const countKey = tagKey + "-count"
+
+        dict[countKey]--
+
+        if (dict[countKey] < 1) {
+          document.head.removeChild(dict[tagKey])
+          delete dict[tagKey]
+          delete dict[countKey]
+        }
+      },
+    })
+  },
+}
+
+if (typeof module !== "undefined") {
+  module.exports = VueComponentCSSMixin
+}
+
+if (typeof window !== "undefined") {
+  window.VueComponentCSSMixin = VueComponentCSSMixin
+}
